6610433cb7165a43d22663a3a78b016838e1d95e
Slava <v4slava.guk@gmail.com>
2025-02-09 21:59:55 +0300

    add docker-compose for db

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI
+
+from core.config import settings
+
+from api import router as api_router
+
+app = FastAPI()
+
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+)
+
+
+@app.get('/')
+def root():
+    return {'message': 'hello world'}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",
+                host=settings.run.host,
+                port=settings.run.port,
+                reload=True
+    )
