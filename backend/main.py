89bc3f7c012adb97e701082b2189d8369729ddd1
Slava <v4slava.guk@gmail.com>
2025-02-10 13:56:12 +0300

    add base for db models

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,32 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
 
 from api import router as api_router
+from core.models import db_helper
 
-app = FastAPI()
 
-app.include_router(
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI()
+
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix
 )
 
-
-@app.get('/')
-def root():
-    return {'message': 'hello world'}
-
-
 if __name__ == "__main__":
-    uvicorn.run("main:app",
+    uvicorn.run("main:main_app",
                 host=settings.run.host,
                 port=settings.run.port,
                 reload=True
-    )
+                )
