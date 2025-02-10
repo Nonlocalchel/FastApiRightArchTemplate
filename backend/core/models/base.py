9723c2a62adde168721609d73d42f55899856442
Slava <v4slava.guk@gmail.com>
2025-02-10 18:20:46 +0300

    upgrade bd

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -1,13 +1,20 @@
+from packaging.metadata import Metadata
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import declared_attr
 from sqlalchemy.orm import mapped_column
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
