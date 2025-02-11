0583322a18026749c76df0b85026abb46ff5e9f9
Slava <v4slava.guk@gmail.com>
2025-02-11 16:20:06 +0300

    create get_access_tokens_db

--- backend/core/config.py
+++ backend/core/config.py
@@ -33,16 +33,18 @@ class DatabaseConfig(BaseModel):
     }
 
 
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=("env.template", ".env"),
-        case_sensitive=False,
-        env_nested_delimiter="__",
-        env_prefix="APP_CONFIG__",
+        env_file=(".env.template", ".env"),
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+    access_token: AccessToken = AccessToken()
 
 
 settings = Settings()
