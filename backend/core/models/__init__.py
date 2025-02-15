b4d1dd2a670e496a55df54491997cda8296f9b23
Slava <v4slava.guk@gmail.com>
2025-02-15 15:46:58 +0300

    restructer

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -5,7 +5,7 @@ __all__ = (
     "AccessToken"
 )
 
-from utils.db_helper import db_helper
+from core.services.db_helper import db_helper
 from .base import Base
 from .access_token import AccessToken
 from .user import User
