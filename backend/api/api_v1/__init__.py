29dd3f4d89ab84f43cdd28baa119d47b5b958792
Slava <v4slava.guk@gmail.com>
2025-02-17 13:00:33 +0300

    repair router(auth header and prefix)

--- backend/api/api_v1/__init__.py
+++ backend/api/api_v1/__init__.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from core.config import settings
-
 from .auth import router as users_router
+from .posts import router as posts_router
 
-router = APIRouter()
+http_bearer = HTTPBearer(auto_error=False)
+
+router = APIRouter(
+    prefix=settings.api.v1.prefix,
+    dependencies=[Depends(http_bearer)],
+)
+
 router.include_router(
     users_router
 )
--- backend/api/api_v1/auth/__init__.py
+++ backend/api/api_v1/auth/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import (
     APIRouter,
-    Depends,
 )
-from fastapi.security import HTTPBearer
 
 from core.config import settings
 
@@ -10,12 +8,7 @@ from .auth import router as auth_router
 from .users import router as users_router
 from .messages import router as messages_router
 
-http_bearer = HTTPBearer(auto_error=False)
-
-router = APIRouter(
-    prefix=settings.api.v1.prefix,
-    dependencies=[Depends(http_bearer)],
-)
+router = APIRouter()
 router.include_router(auth_router)
 router.include_router(users_router)
 router.include_router(messages_router)
